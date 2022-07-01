f8f336178d626fbd1fd854ccfe87c7f9a7f7fb14
wuzhengxuan <wuzhengxuan@hikvision.com.cn>
2022-07-01 18:13:53 +0800

    [ADD] 新增Cover、CoverRow组件

---
+++ src/components/CoverRow.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="cover-row">
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'CoverRow',
+    data() {
+        return {
+            
+        };
+    },
+
+};
+</script>
+<style lang="scss">
+    
+</style>
