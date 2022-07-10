dd40e0d528022af41ca030d2a7bdb0b026c95e5b
wzx <202320062@hdu.edu.cn>
2022-07-10 21:59:41 +0800

    [ADD] CoverRow

--- src/components/CoverRow.vue
+++ src/components/CoverRow.vue
@@ -1,19 +1,145 @@
 <template>
-    <div class="cover-row">
-        
+  <div class="cover-row">
+    <div
+      v-for="item in items"
+      :key="item.id"
+      class="item"
+      :class="{ artist: type === 'artist' }"
+    >
+      <!-- <Cover
+        :id="item.id"
+        :img-url="getImageUrl(item)"
+        :type="type"
+        :play-button-size="type === 'artist' ? 26 : playButtonSize"
+      /> -->
     </div>
+  </div>
 </template>
 <script>
+// import Cover from './Cover.vue';
 export default {
-    name: 'CoverRow',
-    data() {
-        return {
-            
-        };
-    },
+  name: 'CoverRow',
+  components: {
+    // Cover,
+  },
+  props: {
+    items: { type: Array, required: true },
+    type: { type: String, required: true },
+    subText: { type: String, default: 'null' },
+    subTextFontSize: { type: String, default: '16px' },
+    showPlayCount: { type: Boolean, default: false },
+    columnNumber: { type: Number, default: 5 },
+    gap: { type: String, default: '44px 24px' },
+    playButtonSize: { type: Number, default: 22 },
+  },
 
+  data() {
+    return {};
+  },
+
+  computed: {
+    rowStyle() {
+      return {
+        'grid-template-columns': `repeat(${this.columnNumber}, 1fr)`,
+        gap: this.gap,
+      };
+    },
+  },
+  methods: {
+    getImageUrl(item) {
+      if (item.img1v1Url) {
+        let img1v1ID = item.img1v1Url.split('/');
+        img1v1ID = img1v1ID[img1v1ID.length - 1];
+        if (img1v1ID === '5639395138885805.jpg') {
+          // 没有头像的歌手，网易云返回的img1v1Url并不是正方形的
+          return 'https://p2.music.126.net/VnZiScyynLG7atLIZ2YPkw==/18686200114669622.jpg?param=512y512';
+        }
+      }
+      let img = item.img1v1Url || item.picUrl || item.coverImgUrl;
+      return `${img.replace('http://', 'https://')}?param=512y512`;
+    },
+  },
 };
 </script>
 <style lang="scss">
-    
+.cover-row {
+  display: grid;
+}
+
+.item {
+  color: var(--color-text);
+  .text {
+    margin-top: 8px;
+    .title {
+      font-size: 16px;
+      font-weight: 600;
+      line-height: 20px;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+      word-break: break-all;
+    }
+    .info {
+      font-size: 12px;
+      opacity: 0.68;
+      line-height: 18px;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+      word-break: break-word;
+    }
+  }
+}
+
+.item.artist {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  .cover {
+    display: flex;
+  }
+  .title {
+    margin-top: 4px;
+  }
+}
+
+@media (max-width: 834px) {
+  .item .text .title {
+    font-size: 14px;
+  }
+}
+
+.explicit-symbol {
+  opacity: 0.28;
+  color: var(--color-text);
+  float: right;
+  .svg-icon {
+    margin-bottom: -3px;
+  }
+}
+
+.lock-icon {
+  opacity: 0.28;
+  color: var(--color-text);
+  margin-right: 4px;
+  // float: right;
+  .svg-icon {
+    height: 12px;
+    width: 12px;
+  }
+}
+
+.play-count {
+  font-weight: 600;
+  opacity: 0.58;
+  color: var(--color-text);
+  font-size: 12px;
+  .svg-icon {
+    margin-right: 3px;
+    height: 8px;
+    width: 8px;
+  }
+}
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>Hello name:"home", </div>
+  <div v-show="isShow">
+    <div class="index-row">
+      <div class="title">
+        推荐歌单
+        <router-link to="/explore">更多</router-link>
+      </div>
+    </div>
+    <CoverRow />
+  </div>
 </template>
 <script>
+import CoverRow from '../components/CoverRow.vue';
 export default {
   name: 'Home',
-  data() {},
+  components: {
+    CoverRow,
+  },
+  data() {
+    return {
+      isShow: true,
+    };
+  },
 };
 </script>
 <style lang=""></style>
