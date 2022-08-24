f88d012467927ca8aef33867c47da81d53ebcf56
wzx <202320062@hdu.edu.cn>
2022-08-24 14:23:05 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -44,6 +44,7 @@ module.exports = {
     },
   },
   chainWebpack(config) {
+    config.resolve.alias.set('@', resolve('src')); // key,value自行定义，比如.set('@@', resolve('src/components'))
     config.module.rules.delete('svg');
     config.module.rule('svg').exclude.add(resolve('src/assets/icons')).end();
     config.module
