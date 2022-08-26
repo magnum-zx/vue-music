d6061f0f41a27224aecc7d29a413ea2782ed2369
wzx <202320062@hdu.edu.cn>
2022-08-26 16:48:16 +0800

    dist

---
+++ dist/precache-manifest.c54db6aea28eb9314b4bd41b0de7ca9b.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9b6da0c4e6abcd355d",
+    "url": "/css/chunk-2d358d7e.9ae61daa.css"
+  },
+  {
+    "revision": "de975be1ce9dd31ea892",
+    "url": "/css/chunk-vendors.46ab30d4.css"
+  },
+  {
+    "revision": "94f8a8a29f43c8f5931d",
+    "url": "/css/index.941145d7.css"
+  },
+  {
+    "revision": "8f052dfd88c2dd49c20092719a5eefa2",
+    "url": "/fonts/Barlow-Black.8f052dfd.woff2"
+  },
+  {
+    "revision": "be1a02d196679b20c6c882d6a7ff1caf",
+    "url": "/fonts/Barlow-Black.be1a02d1.ttf"
+  },
+  {
+    "revision": "59a26a7b9fd3aa03aa096e3423655f09",
+    "url": "/fonts/Barlow-Bold.59a26a7b.ttf"
+  },
+  {
+    "revision": "692f76d258209ce2e576c09bfd4e007f",
+    "url": "/fonts/Barlow-Bold.692f76d2.woff2"
+  },
+  {
+    "revision": "2cde6ca8fc0b1d84400376ac3b919810",
+    "url": "/fonts/Barlow-ExtraBold.2cde6ca8.ttf"
+  },
+  {
+    "revision": "8ca45b6125513523b93e198bfe4e9edb",
+    "url": "/fonts/Barlow-ExtraBold.8ca45b61.woff2"
+  },
+  {
+    "revision": "76624a4ceb8e9f64645c398f1787ad7a",
+    "url": "/fonts/Barlow-Medium.76624a4c.woff2"
+  },
+  {
+    "revision": "e68f05546a44a68b911db03f4d0b880f",
+    "url": "/fonts/Barlow-Medium.e68f0554.ttf"
+  },
+  {
+    "revision": "9d1fe92475ac7f40348941af959f60e3",
+    "url": "/fonts/Barlow-Regular.9d1fe924.ttf"
+  },
+  {
+    "revision": "fa0dbecb86eaa269b09241d65baee6db",
+    "url": "/fonts/Barlow-Regular.fa0dbecb.woff2"
+  },
+  {
+    "revision": "641297f4cc0074b22c3f23e22ddde9ec",
+    "url": "/fonts/Barlow-SemiBold.641297f4.woff2"
+  },
+  {
+    "revision": "d41a276837f8f092b4e9bcd26a032fa2",
+    "url": "/fonts/Barlow-SemiBold.d41a2768.ttf"
+  },
+  {
+    "revision": "a1cef3d530e1adb0f52b2f62994a2aca",
+    "url": "/fonts/codicon.a1cef3d5.ttf"
+  },
+  {
+    "revision": "15882a7ddc67f235e6bfc39fba60c040",
+    "url": "/img/logos/lastfm.png"
+  },
+  {
+    "revision": "e1e193ddd5b29a6605178bdd8d134246",
+    "url": "/img/logos/netease-music.png"
+  },
+  {
+    "revision": "94558d81359fe2044cd9e7067a8a3cae",
+    "url": "/img/logos/nyancat-stop.png"
+  },
+  {
+    "revision": "2822cbcf68de083b96ac3921d0e308a2",
+    "url": "/img/logos/nyancat.gif"
+  },
+  {
+    "revision": "11287de66a2e9b9877226f9fb5133cec",
+    "url": "/img/logos/yesplaymusic-white24x24.png"
+  },
+  {
+    "revision": "e099cb8ea828a912212f9022b96d248f",
+    "url": "/img/logos/yesplaymusic.png"
+  },
+  {
+    "revision": "299c141bf8b002f92e2d69df09fd226e",
+    "url": "/img/touchbar/backward.png"
+  },
+  {
+    "revision": "ab6f327d21116dd60adb02cadf8bcadf",
+    "url": "/img/touchbar/forward.png"
+  },
+  {
+    "revision": "f46e5d6308ff6d7b3c1ab74fe8f2c2a7",
+    "url": "/img/touchbar/like.png"
+  },
+  {
+    "revision": "9907a7f0207de6912daedc63353286a4",
+    "url": "/img/touchbar/like_fill.png"
+  },
+  {
+    "revision": "24007c3004310acf19e1a51ab4173541",
+    "url": "/img/touchbar/next_up.png"
+  },
+  {
+    "revision": "5e589bdadbd1513b0eac82cb58df2855",
+    "url": "/img/touchbar/page_next.png"
+  },
+  {
+    "revision": "8f3988bf4b0590ce24e248ecb946f3e3",
+    "url": "/img/touchbar/page_prev.png"
+  },
+  {
+    "revision": "fdafd5ac8a24471ff0270f6329495d0d",
+    "url": "/img/touchbar/pause.png"
+  },
+  {
+    "revision": "f4c8e812e62e581618c83fc277a7294b",
+    "url": "/img/touchbar/play.png"
+  },
+  {
+    "revision": "42df6d536c759fd26ad76d72c8eb533d",
+    "url": "/img/touchbar/search.png"
+  },
+  {
+    "revision": "81e4b1a17dd322bbd43fb50cf3576328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d9b6da0c4e6abcd355d",
+    "url": "/js/chunk-2d358d7e.295ce70f.js"
+  },
+  {
+    "revision": "de975be1ce9dd31ea892",
+    "url": "/js/chunk-vendors.bc95d4d4.js"
+  },
+  {
+    "revision": "94f8a8a29f43c8f5931d",
+    "url": "/js/index.d78349c3.js"
+  },
+  {
+    "revision": "ed3338bff27e55ef8525cd2797149689",
+    "url": "/manifest.json"
+  }
+]);
